a1ff585e31ac8944de1d638e4abb5c7adb3987ee
unknown <leonardononatobs@gmail.com>
2026-02-24 09:54:50 -0300

    Entrada de dados e condicionais

---
+++ aula.py
@@ -0,0 +1,13 @@
+nomero = float(input("informe um nomero: "))
+if nomero<=100:
+    print("boa 👍")
+else:
+    print(nomero/2)
+
+#---------------------------------------------------------------------------------------
+
+nomer = int(input("dá um nomero entero ai: "))
+if nomer%2==0:
+    print("iso ai é par 🫃")
+else:
+    print("iso é impa 🧑‍💻")
